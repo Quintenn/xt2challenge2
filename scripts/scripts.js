1b20f63ff5311ab42b8dc568a006b5d3feeae087
Quinten <quintenvanl@gmail.com>
2020-03-26 17:01:11 +0100

    added clock

---
+++ scripts/scripts.js
@@ -0,0 +1,74 @@
+
+/**
+ * Start clock
+ */
+function initClock() {
+
+	// init
+	renderClock();
+
+	// run every second
+	setInterval(renderClock, 1000);
+}
+
+/**
+ * Render clock
+ */
+function renderClock() {
+
+	// get HTML elements
+	var clock = document.getElementById('time');
+	var date  = document.getElementById('date');
+
+	// time logic
+	var time  = getCurrentTime(new Date());
+	var sep   = flashSeperator(time['seconds']);
+
+	// display date
+    date.innerHTML = getCurrentMonth(time['month']) + ' ' +  time['day'];
+
+	// display time
+	clock.innerHTML = time['hours'] + sep +  time['minutes'];
+}
+
+/**
+ * Parse the time
+ * @param date object - current time 
+ * @return date array
+ */
+function getCurrentTime(date) {
+	var time = [];
+
+	// fill array
+	time['seconds'] = date.getSeconds();
+	time['minutes'] = date.getMinutes(),
+	time['hours']   = date.getHours();
+	time['month']   = date.getMonth();
+	time['day']     = date.getDay();
+
+	// hours: add leading zero
+	if (time['hours'] < 10) {
+		time['hours'] = '0' + time['hours'];
+	}
+
+	// minutes: add leading zero
+	if (time['minutes'] < 10) {
+		time['minutes'] = '0' + time['minutes'];
+	}
+
+	return time;
+}
+
+/**
+ * Get current Month
+ * @param integer - month number
+ * @return string
+ */
+function getCurrentMonth(monthNumber) {
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+	return months[monthNumber];
+}
+
+// start
+initClock();
